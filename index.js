16d13e33a06fd1dbc6cf378909e81b8e2c053d15
zeroskillsamus <abemitch06500@hotmail.com>
2024-11-13 08:05:09 -0600

    Changed port number

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
-import express from "express";
-import { getvidsrc } from "./src/vidsrcpro.js";
-import { getasiaheroku } from "./src/asiaheroku.js";
+/** @format */
 
-const port = 3000;
+import express from 'express'
+import { getvidsrc } from './src/vidsrcpro.js'
+import { getasiaheroku } from './src/asiaheroku.js'
+
+const port = 5000
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        intro: "Welcome to the unofficial vidsrcPro provider",
-        routes: {
-            movie: "/vidsrc/:movieTMDBid",
-            show: "/vidsrc/:showTMDBid?s=seasonNumber&e=episodeNumber"
-        },
-        author: "This api is developed and created by Inside4ndroid Studios"
-    });
-});
+	res.status(200).json({
+		intro: 'Welcome to the unofficial vidsrcPro provider',
+		routes: {
+			movie: '/vidsrc/:movieTMDBid',
+			show: '/vidsrc/:showTMDBid?s=seasonNumber&e=episodeNumber',
+		},
+		author: 'This api is developed and created by Inside4ndroid Studios',
+	})
+})
 
 app.get('/vidsrc/:tmdbId', async (req, res) => {
-    const id = req.params.tmdbId;
-    const season = req.query.s;
-    const episode = req.query.e;
+	const id = req.params.tmdbId
+	const season = req.query.s
+	const episode = req.query.e
 
-    try {
-        if (season && episode) {
-            const vidsrcresponse = await getvidsrc(id, season, episode);
-            res.status(200).json(vidsrcresponse);
-        } else {
-            const vidsrcresponse = await getvidsrc(id);
-            res.status(200).json(vidsrcresponse);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-});
+	try {
+		if (season && episode) {
+			const vidsrcresponse = await getvidsrc(id, season, episode)
+			res.status(200).json(vidsrcresponse)
+		} else {
+			const vidsrcresponse = await getvidsrc(id)
+			res.status(200).json(vidsrcresponse)
+		}
+	} catch (error) {
+		console.error('Error fetching data:', error)
+		res.status(500).json({ error: 'Failed to fetch data' })
+	}
+})
 
 app.get('/asiaheroku/:tmdbId', async (req, res) => {
-    const id = req.params.tmdbId;
-    const season = req.query.s;
-    const episode = req.query.e;
+	const id = req.params.tmdbId
+	const season = req.query.s
+	const episode = req.query.e
 
-    try {
-        if (season && episode) {
-            const srcresponse = await getasiaheroku(id, season, episode);
-            res.status(200).json(srcresponse);
-        } else {
-            const vidsrcresponse = await getasiaheroku(id);
-            res.status(200).json(vidsrcresponse);
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
-});
+	try {
+		if (season && episode) {
+			const srcresponse = await getasiaheroku(id, season, episode)
+			res.status(200).json(srcresponse)
+		} else {
+			const vidsrcresponse = await getasiaheroku(id)
+			res.status(200).json(vidsrcresponse)
+		}
+	} catch (error) {
+		console.error('Error fetching data:', error)
+		res.status(500).json({ error: 'Failed to fetch data' })
+	}
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
-});
+	console.log(`Example app listening on port http://localhost:${port}`)
+})
